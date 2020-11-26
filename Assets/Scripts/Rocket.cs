b8dc80a277286514912d99603d27130721fbaab6
Michael Campos <camposmj89@gmail.com>
2020-11-26 08:58:13 -0800

    Add new obstacles

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+    [SerializeField] private float _rcsThrust = 100f;
+    [SerializeField] private float _mainThrust = 100f;
+    
+    // Config
     private Rigidbody _rigidbody;
     private AudioSource _audioSource;
-    
+
     // Start is called before the first frame update
     void Start() {
         _rigidbody = GetComponent<Rigidbody>();
@@ -14,12 +19,13 @@ public class Rocket : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
-            _rigidbody.AddRelativeForce(Vector3.up);
+            _rigidbody.AddRelativeForce(Vector3.up * _mainThrust * Time.deltaTime);
 
             if (!_audioSource.isPlaying) {
                 _audioSource.Play();
@@ -27,12 +33,31 @@ public class Rocket : MonoBehaviour {
         } else {
             _audioSource.Stop();
         }
+    }
 
+    private void Rotate() {
+        float rotationSpeed = _rcsThrust * Time.deltaTime;
+        
+        _rigidbody.freezeRotation = true;
+        
         // You can hold either A or D
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         } else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationSpeed);
+        }
+
+        _rigidbody.freezeRotation = false;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        switch (other.transform.tag) {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            default:
+                Debug.Log("Not friendly");
+                break;
         }
     }
 }
